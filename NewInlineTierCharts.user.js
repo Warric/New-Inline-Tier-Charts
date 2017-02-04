338a284c3cfcccb831c16802c9f225f921154625
Björn Büttner <Idrinth@users.noreply.github.com>
2017-02-04 11:40:03 +0100

    Update NewInlineTierCharts.user.js

--- NewInlineTierCharts.user.js
+++ NewInlineTierCharts.user.js
@@ -41,7 +41,7 @@
         if ( src ) {
             wrapper.appendChild ( document.createElement('div') );
             wrapper.firstChild.appendChild ( document.createElement ( "img" ) );
-            wrapper.firstChild.firstChild.setAttribute ( "src", "http://image.prntscr.com/image/" + src + ".png" );
+            wrapper.firstChild.firstChild.setAttribute ( "src", src );
             wrapper.firstChild.firstChild.setAttribute ( "alt", label );
         }
         if(label) {
@@ -95,27 +95,27 @@
 } ) (
         [
             [ "", "" ],
-            [ "Z1-9", "4f72335bf6a44a699472fb174e487a28" ],
-            [ "Small", "ebe0a58b24d9468ab735f7b588129daa" ],
-            [ "Med", "663ac3e4731e4aa7b7ade1f8e1b95251" ],
-            [ "Large", "7e9b76a799e5409a8ae7413d6ac48375" ],
-            [ "Epic", "3ca7397851fe48dbbde979e5e9660726" ],
-            [ "Colo", "32ca6648468648dc83269be64f5f9c78" ],
-            [ "Giga", "aec4152d67c943ada2f1a9658d6fa750" ],
-            [ "Elite", "4dd259053e5a42f590523448797b52d4" ],
+            [ "Z1-9", "http://image.prntscr.com/image/4f72335bf6a44a699472fb174e487a28.png" ],
+            [ "Small", "http://image.prntscr.com/image/ebe0a58b24d9468ab735f7b588129daa.png" ],
+            [ "Med", "http://image.prntscr.com/image/663ac3e4731e4aa7b7ade1f8e1b95251.png" ],
+            [ "Large", "http://image.prntscr.com/image/7e9b76a799e5409a8ae7413d6ac48375.png" ],
+            [ "Epic", "http://image.prntscr.com/image/3ca7397851fe48dbbde979e5e9660726.png" ],
+            [ "Colo", "http://image.prntscr.com/image/32ca6648468648dc83269be64f5f9c78.png" ],
+            [ "Giga", "http://image.prntscr.com/image/aec4152d67c943ada2f1a9658d6fa750.png" ],
+            [ "Elite", "http://image.prntscr.com/image/4dd259053e5a42f590523448797b52d4.png" ],
             [ "", "" ],
-            [ "Old Guild", "6afdca29e2f14617ad6ee220615ebc54" ],
-            [ "Guild 1/2", "ecb8c73134954e61ad5990a476cd78d4" ],
-            [ "Guild 2/2", "7df09c5b6cd8466dadb589a1ec0f91e8" ],
+            [ "Old Guild", "http://image.prntscr.com/image/6afdca29e2f14617ad6ee220615ebc54.png" ],
+            [ "Guild 1/2", "http://image.prntscr.com/image/ecb8c73134954e61ad5990a476cd78d4.png" ],
+            [ "Guild 2/2", "http://image.prntscr.com/image/7df09c5b6cd8466dadb589a1ec0f91e8.png" ],
             [ "", "" ],
-            [ "BoB Map", "f1b3999996334f42b293630ecf283bc9" ],
-            [ "MaM Map", "dd378a271f5e40979f2638f444ee0e7d" ],
-            [ "GD Map", "61350f40209243ffa416a3cd7fbe4931" ],
-            [ "GoC Map", "333019c25a0e435593a15ef78986539b" ],
-            [ "FW Map", "4d0e0f3a7d30407887e3e323ee5350c4" ],
-            [ "NQ Map", "14a4b5c29a0b4a0596f909c481050c02" ],
-            [ "RT Map", "10c56af4977c4bc19a128290a5405f83" ],
+            [ "BoB Map", "http://image.prntscr.com/image/f1b3999996334f42b293630ecf283bc9.png" ],
+            [ "MaM Map", "http://image.prntscr.com/image/dd378a271f5e40979f2638f444ee0e7d.png" ],
+            [ "GD Map", "http://image.prntscr.com/image/61350f40209243ffa416a3cd7fbe4931.png" ],
+            [ "GoC Map", "http://image.prntscr.com/image/333019c25a0e435593a15ef78986539b.png" ],
+            [ "FW Map", "http://image.prntscr.com/image/4d0e0f3a7d30407887e3e323ee5350c4.png" ],
+            [ "NQ Map", "http://image.prntscr.com/image/14a4b5c29a0b4a0596f909c481050c02.png" ],
+            [ "RT Map", "http://image.prntscr.com/image/10c56af4977c4bc19a128290a5405f83.png" ],
             [ "", "" ],
-            [ "WR", "ba2b52e1a1fd47ee9afd1686f7481416" ]
+            [ "WR", "http://image.prntscr.com/image/ba2b52e1a1fd47ee9afd1686f7481416.png" ]
         ]
 );
