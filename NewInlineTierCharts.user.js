772a8744ef292417a35e94378e25879787f52aee
Warric <warric1993@gmail.com>
2017-03-26 23:43:23 +0200

    Update NewInlineTierCharts.user.js

--- NewInlineTierCharts.user.js
+++ NewInlineTierCharts.user.js
@@ -23,7 +23,7 @@
 // @include        http://armorgames.com/dawn-of-the-dragons-game/13509*
 // @include        http://www.armorgames.com/dawn-of-the-dragons-game/13509*
 // @include        http://armorgames.com/play/13509/dawn-of-the-dragons*
-// @version        3.5.9
+// @version        3.5.10
 // @grant          none
 // ==/UserScript==
 ( function ( data ) {
